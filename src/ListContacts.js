36cb6b5fa73529845e8ea60a543bdd9d9b8b6982
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-10-08 19:53:53 +0100

    Add form for seach contacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,18 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class ListContacts extends Component {
+class ListContacts extends Component{
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired,
+  }
+  state ={
+    query: ''
+  }
+  handleChange(e){
+    this.setState((prevState)=>({
+      query: e.target.value
+    }))
+  }
+
   render() {
-    const contacts = this.props.contacts
-    console.log(contacts);
     return (
-      <div>
+      <div className="list-contacts">
+        <div className="list-contact-top">
+          <input type="text" className="search-contacts" placeholder="Search Contacts" value={this.state.query} onChange={this.handleChange} />
+        </div>
         <ol className="contact-list">
-          {contacts.map(contact => (
-            <li key={contact.id}>{contact.name}</li>
-          ))}
+            {this.props.contacts.map(contact => (
+              <li key={contact.id} className='contact-list-item'>
+                <div className="contact-avatar" 
+                style={{
+                  backgroundImage: `url(${contact.avatarURL})`
+                }}>
+                </div>
+                <div className="contact-details">
+                  <p>{contact.name}</p>
+                  <p>@{contact.handle}</p>
+                </div>
+                <button onClick={() => this.props.onDeleteContact(contact)} className="contact-remove">X</button>
+              </li>
+            ))}
         </ol>
       </div>
     )
   }
 }
+
 export default ListContacts;
